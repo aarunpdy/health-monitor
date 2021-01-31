be355a2eb4079022dd9378b75d8fdd8afd30ff29
Arun A <arunarumugam@paypal.com>
2021-02-01 00:30:58 +0530

    home activity added

--- app/src/main/java/com/example/healthmonitor/ui/fragments/LoginFragment.java
+++ app/src/main/java/com/example/healthmonitor/ui/fragments/LoginFragment.java
@@ -1,17 +1,13 @@
 package com.example.healthmonitor.ui.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.EditorInfo;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -20,9 +16,19 @@ import androidx.annotation.StringRes;
 import androidx.fragment.app.Fragment;
 
 import com.example.healthmonitor.R;
+import com.example.healthmonitor.databinding.FragmentLoginBinding;
+import com.example.healthmonitor.ui.activities.HomeActivity;
 
 
 public class LoginFragment extends Fragment {
+
+    enum LoginState {
+        LOG_IN,
+        SIGN_UP;
+    }
+
+    private LoginState loginState = LoginState.LOG_IN;
+    private FragmentLoginBinding binding;
 
     @Nullable
     @Override
@@ -35,11 +41,27 @@ public class LoginFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        binding = FragmentLoginBinding.bind(view);
 
-        final EditText usernameEditText = view.findViewById(R.id.username);
-        final EditText passwordEditText = view.findViewById(R.id.password);
-        final Button loginButton = view.findViewById(R.id.login);
-        final ProgressBar loadingProgressBar = view.findViewById(R.id.loading);
+        prepareView();
+    }
+
+    private void prepareView() {
+        if (loginState == LoginState.LOG_IN) {
+            binding.username.setVisibility(View.GONE);
+            binding.usernameText.setVisibility(View.GONE);
+
+            binding.loginButton.setText(R.string.action_sign_in);
+            binding.signUpButton.setText(R.string.action_sign_up);
+            binding.signUpText.setText(R.string.create_account_desc);
+        } else {
+            binding.username.setVisibility(View.VISIBLE);
+            binding.usernameText.setVisibility(View.VISIBLE);
+
+            binding.loginButton.setText(R.string.action_sign_up);
+            binding.signUpButton.setText(R.string.action_sign_in);
+            binding.signUpText.setText(R.string.login_account_desc);
+        }
 
         TextWatcher afterTextChangedListener = new TextWatcher() {
             @Override
@@ -54,29 +76,52 @@ public class LoginFragment extends Fragment {
 
             @Override
             public void afterTextChanged(Editable s) {
-
+                // TODO: validate input data
             }
         };
-        usernameEditText.addTextChangedListener(afterTextChangedListener);
-        passwordEditText.addTextChangedListener(afterTextChangedListener);
-        passwordEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
 
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if (actionId == EditorInfo.IME_ACTION_DONE) {
-
-                }
-                return false;
+        binding.username.addTextChangedListener(afterTextChangedListener);
+        binding.passwordEditText.addTextChangedListener(afterTextChangedListener);
+        binding.passwordEditText.setOnEditorActionListener((v, actionId, event) -> {
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                doFormAction();
             }
+            return false;
         });
 
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                loadingProgressBar.setVisibility(View.VISIBLE);
-
-            }
+        binding.loginButton.setOnClickListener(v -> {
+            doFormAction();
         });
+
+        binding.signUpButton.setOnClickListener(v -> {
+            if (loginState == LoginState.SIGN_UP) {
+                loginState = LoginState.LOG_IN;
+            } else {
+                loginState = LoginState.SIGN_UP;
+            }
+
+            prepareView();
+        });
+    }
+
+    private void doFormAction() {
+        binding.loading.setVisibility(View.VISIBLE);
+        if (loginState == LoginState.SIGN_UP) {
+            // TODO: make sign up call
+        } else {
+            // TODO: make login call
+        }
+        // on success operation
+    }
+
+    private void onLoginSuccess() {
+        startActivity(new Intent(getActivity(), HomeActivity.class));
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        binding = null;
     }
 
     private void showLoginFailed(@StringRes Integer errorString) {
