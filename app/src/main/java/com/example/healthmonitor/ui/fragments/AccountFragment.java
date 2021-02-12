f0a67e83c5dd05763dd37e69697eacbb143a512a
Arun A <arunarumugam@paypal.com>
2021-02-12 18:06:24 +0530

    profile screen added

--- app/src/main/java/com/example/healthmonitor/ui/fragments/AccountFragment.java
+++ app/src/main/java/com/example/healthmonitor/ui/fragments/AccountFragment.java
@@ -1,22 +1,59 @@
 package com.example.healthmonitor.ui.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.healthmonitor.R;
+import com.example.healthmonitor.databinding.FragmentAccountBinding;
+import com.example.healthmonitor.ui.activities.LoginActivity;
 
 public class AccountFragment extends Fragment {
 
+    private FragmentAccountBinding binding;
+    private boolean isEditMode = false;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_account, container, false);
-        final TextView textView = root.findViewById(R.id.text_notifications);
-        return root;
+        return inflater.inflate(R.layout.fragment_account, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding = FragmentAccountBinding.bind(view);
+
+        updateUI();
+    }
+
+    private void updateUI() {
+        binding.emailEditText.setEnabled(isEditMode);
+        binding.nameEditText.setEnabled(isEditMode);
+        binding.btnEdit.setText(isEditMode ? "Save" : "Edit");
+        binding.btnEdit.setOnClickListener(v -> {
+            isEditMode = !isEditMode;
+            updateUI();
+        });
+
+        binding.btnLogout.setOnClickListener(v -> {
+            Intent intent = new Intent(getActivity(), LoginActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+        });
+
+        binding.btnSend.setOnClickListener(v -> {
+            uploadData();
+        });
+    }
+
+    private void uploadData() {
+    }
+
+
 }
